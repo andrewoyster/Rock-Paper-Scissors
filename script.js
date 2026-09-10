ec665aeabf3d0b3b332fcf433a1c88cf98b2b3a1
andrew.oyster <aoyster26@gmail.com>
2026-09-10 15:30:41 -0400

    create human and computer choice. Create game logic and score

--- script.js
+++ script.js
@@ -4,8 +4,8 @@ let humanScore = 0;
 let computerScore = 0;
 
 function getComputerChoice() {
-  const randomPCChoice = Math.floor(Math.random() * choices.length);
-  return choices[randomPCChoice];
+  const randomComputerChoice = Math.floor(Math.random() * choices.length);
+  return choices[randomComputerChoice];
 }
 
 function getHumanChoice() {
@@ -13,4 +13,22 @@ function getHumanChoice() {
   return userInput;
 }
 
-console.log(getPlayerChoice());
+function playRound(humanChoice, computerChoice) {
+  humanChoice = humanChoice.toLowerCase();
+
+  if (humanChoice === computerChoice) {
+    console.log("You tied!");
+  } else if (
+    (humanChoice === "rock" && computerChoice === "scissors") ||
+    (humanChoice === "paper" && computerChoice === "rock") ||
+    (humanChoice === "scissors" && computerChoice === "paper")
+  ) {
+    console.log("The Human Wins");
+    humanScore++;
+  } else {
+    console.log("Computer Wins");
+    computerScore++;
+  }
+}
+
+playRound(getHumanChoice(), getComputerChoice());
